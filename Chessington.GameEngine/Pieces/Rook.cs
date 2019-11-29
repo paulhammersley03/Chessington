1112679e947e612877c2586fd87a2a6454b746bb
Paul Hammersley <P.Hammersley@ucas.ac.uk>
2019-11-29 12:10:39 +0000

    Rook move working and passes test

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -6,11 +7,21 @@ namespace Chessington.GameEngine.Pieces
     public class Rook : Piece
     {
         public Rook(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+            for (int i = 0; i < GameSettings.BoardSize; i++)
+            {
+                availableMoves.Add(Square.At(currentSquare.Row, i));
+                availableMoves.Add(Square.At(i, currentSquare.Col));
+            }
+            availableMoves.RemoveAll(x => x == currentSquare);
+            return availableMoves;
         }
     }
 }
